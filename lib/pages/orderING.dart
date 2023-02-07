36e24265e76b526b64a77b1306d424bb6d92c6d4
hides <hidessam80@gmail.com>
2023-02-07 23:16:26 +0900

    코멘트

--- lib/pages/orderING.dart
+++ lib/pages/orderING.dart
@@ -181,7 +181,9 @@ class _MyAppState extends State<orderING> {
               UpdateData.confirmYNChange(
                   ordIndex, userCarNoController.text, 'Y');
               Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => third()));
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => third())); //배차 내역으로 화면 이동
               refresh();
             } else {
               Fluttertoast.showToast(msg: '차량 번호를 입력해주세요');
